ed1a017c22a950b8b5152856210ac273afe2855c
Viktor Lumiste <viktor.lumiste@khk.ee>
2022-04-05 09:30:58 +0300

    API abiga andmete haaramine

--- API andmete haaraja.py
+++ API andmete haaraja.py
@@ -1,1 +1,5 @@
-from requests import request
+import requests
+url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=100000"
+response = requests.get(url)
+print("Status code: ", response.status_code)
+response_dict = response.json()
