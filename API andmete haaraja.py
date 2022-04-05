f750f0948db98203b2087a08ecc4fa3645a69e80
Viktor Lumiste <viktor.lumiste@khk.ee>
2022-04-05 11:07:20 +0300

    Teeme JSON file andmetega [delivers #181785855]

--- API andmete haaraja.py
+++ API andmete haaraja.py
@@ -1,5 +1,15 @@
 import requests
+import json
 url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=100000"
 response = requests.get(url)
 print("Status code: ", response.status_code)
 response_dict = response.json()
+print(response_dict)
+for poke in response_dict["results"]:
+    pokemon = json.dumps(poke, indent=4)
+    with open("pokemonid.json", "a") as outfile:
+        outfile.write(pokemon)
+        outfile.write(", \n")
+
+    print(poke)
+    print(pokemon)
