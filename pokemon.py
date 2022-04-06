4612a52b9cc2aebfa862d0074ecf72c0de169842
Viktor Lumiste <viktor.lumiste@khk.ee>
2022-04-06 11:28:09 +0300

    pain

--- pokemon.py
+++ pokemon.py
@@ -1,16 +1,46 @@
 import json
-
 import requests
+class Pokemon():
+     input_name = input("Sisestage pokemoni nimi: ").lower()
+     with open('pokemonid.json') as json_file:
+          data = json.load(json_file)
+          i = 0
+     for poke in data:
+          if input_name == data[i]["name"]:
+               pokeurl = (data[i]["url"])
+               i += 1
+          else:
+               i += 1
+     response = requests.get(pokeurl)
+     response_dict = response.json()
+     failname = input_name + ".json"
+     andmed = requests.get(pokeurl).json()
+     with open(failname, 'w') as json_file:
+          json.dump(andmed, json_file)
+     tyybid = andmed["types"]
+     if len(tyybid) > 1:
+          types1 = tyybid[0]["type"]
+          types2 = tyybid[1]["type"]
+     else:
+          types1 = tyybid[0]["type"]
+          types2 = ""
+     def getTypes(self, tyyp1, tyyp2):
+          if tyyp2 != "":
+               response1 = requests.get(self.types1["url"])
+               response_dict1 = response1.json()
+               response2 = requests.get(self.types2["url"])
+               response_dict2 = response2.json()
+               print(response_dict1["damage_relations"])
+               print(response_dict2["damage_relations"])
+          else:
+                response1 = requests.get(self.types1["url"])
+                response_dict1 = response1.json()
+                print(response_dict1["damage_relations"])
+     def getMultiplier(self,poke1, poke2):
+          for typ in self.poke1.response_dict1:
+               for a in typ:
+                    if a["name"] == poke2.types1
+poke1 = Pokemon()
+poke1.getTypes(poke1.types1, poke1.types2)
 
-#class Pokemon():
-with open('pokemonid.json') as json_file:
-     data = json.load(json_file)
-     # Print the type of data variable
-     print("Type:", type(data))
-
-
-     # Print the data of dictionary
-#     print("\nPeople1:", data['url'])
-#     print("\nPeople2:", data['name'])
-#poke1 = Pokemon()
-print(data[0]['url'])
+#print(poke1.data[0]['url'])
